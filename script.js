49452bbb186daa802cfbaab61f19435e49a50f7e
amal <121586027+AmalKrishnaS@users.noreply.github.com>
2024-07-20 20:25:54 +0530

    Add functionality to resize and reset the grid - Implemented functionality for the resize and reset button - Refactored code to remove global variables for improved security   and maintainability

--- script.js
+++ script.js
@@ -1,26 +1,61 @@
-const parentDiv = document.querySelector('#grid-container');
-const squareDiv = parentDiv.children; 
+function createGrid(dimension=100) {
+    const parentDiv = document.querySelector('#grid-container');
+    for (let i=1; i<=dimension; i++) {
+        for (let i=1; i<=dimension; i++) {
+            const childDiv = document.createElement('div');
+            childDiv.style.cssText = `width: ${600/dimension}px; height:${600/dimension}px;`;
 
-
-function CreateGrid(parentDiv) {
-    for (let i=1; i<=16; i++) {
-        for (let i=1; i<=16; i++) {
-            const div = document.createElement('div');
-            div.style.flexBasis = '60px';
-
-            parentDiv.appendChild(div);
+            parentDiv.appendChild(childDiv);
         }
     }
 }
 
 function handleHover(event){
-    for (let i=0; i<squareDiv.length; i++) {
-        if (event.target === squareDiv.item(i)) {
-            squareDiv.item(i).style.backgroundColor = 'black';
+    const parentDiv = document.querySelector('#grid-container');
+    const childDiv = parentDiv.children;
+    for (let i=0; i<childDiv.length; i++) {
+        if (event.target === childDiv.item(i)) {
+            childDiv.item(i).style.backgroundColor = 'black';
         }
     }
 }
 
-CreateGrid(parentDiv);
+function resetAndResizeBoard() {
+    const parentDiv = document.querySelector('#grid-container');
+    const childDiv = parentDiv.children;
 
-parentDiv.addEventListener('mouseover', handleHover);
+    function getDimension() {
+        let dimension = +prompt('Enter dimension');
+        while (!dimension || dimension<1 || dimension>100) {
+            dimension = +prompt('invalid value, re-enter');
+        }
+        return dimension;
+    }
+    
+    function removeGrid() {
+        while(parentDiv.firstChild) {
+            parentDiv.removeChild(parentDiv.firstChild);
+        }
+    }
+
+    const dimension = getDimension();
+    removeGrid();
+    console.log(childDiv.length);
+    createGrid(dimension);
+    console.log(childDiv.length);  
+}
+
+function addEventListenerHover() {
+    const parentDiv = document.querySelector('#grid-container');
+    parentDiv.addEventListener('mouseover', handleHover);
+}
+
+function addEventListenerReset() {
+    const resizeButton  = document.querySelector('#resize-button');
+    resizeButton.addEventListener('click', resetAndResizeBoard);
+}
+
+
+createGrid();
+addEventListenerHover();
+addEventListenerReset();
